2b963e8166280ef3e3b684104993fd230dde1f57
Syeda Sana <sanashaaah4@gmail.com>
2023-08-30 01:01:10 -0700

    basic example of TextView

--- app/src/main/java/com/example/kotlin_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_app/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.kotlin_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var txt= findViewById<TextView>(R.id.textView2)
     }
 }
