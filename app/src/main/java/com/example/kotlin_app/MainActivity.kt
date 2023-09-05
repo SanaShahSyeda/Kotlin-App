8a09de08adcdbbf197021801b341ea621c5c4b19
Syeda Sana <sanashaaah4@gmail.com>
2023-09-05 00:30:35 -0700

    basic example of TextView

--- app/src/main/java/com/example/kotlin_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_app/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.kotlin_app
 
-import androidx.appcompat.app.AppCompatActivity
+//Please write your app's package name correctly on the first line
+
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var txt= findViewById<TextView>(R.id.textView2)
+        val txt1 = findViewById<TextView>(R.id.txt1)
+        val btn = findViewById<Button>(R.id.button1)
+        btn.setOnClickListener{
+            txt1.text = "You clicked the button"
+        }
     }
 }
