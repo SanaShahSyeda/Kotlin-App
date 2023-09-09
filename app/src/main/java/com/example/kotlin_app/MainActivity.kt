34ddfdaf717093525d3e7d019b0bf34119e8fa57
Syeda Sana <sanashaaah4@gmail.com>
2023-09-09 04:08:03 -0700

    A hello Toast task and Image changing app with radio buttons

--- app/src/main/java/com/example/kotlin_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_app/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.kotlin_app
 
-//Please write your app's package name correctly on the first line
-
-import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.compose.ui.res.stringResource
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val txt1 = findViewById<TextView>(R.id.txt1)
-        val btn = findViewById<Button>(R.id.button1)
-        btn.setOnClickListener{
-            txt1.text = "You clicked the button"
-        }
+        var count=0
+        var toastbtn = findViewById<Button>(R.id.toastbtn)
+        var countbtn = findViewById<Button>(R.id.countbtn)
+        var counter  = findViewById<TextView>(R.id.counter)
+        toastbtn.setOnClickListener(View.OnClickListener {
+            Toast.makeText(applicationContext, "Hello Toast", Toast.LENGTH_SHORT).show()
+        })
+        countbtn.setOnClickListener(View.OnClickListener {
+            count++;
+            counter.setText("$count")
+        })
     }
 }
