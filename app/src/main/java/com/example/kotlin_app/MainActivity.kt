41f1d12e5153ccdf2c9ddb53c33bc0ce2034ddce
Syeda Sana <sanashaaah4@gmail.com>
2023-09-16 01:17:00 -0700

    a basic profile app with 2 actiivties using explicit intent with edit in activity1 and saved state shown in activity2

--- app/src/main/java/com/example/kotlin_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_app/MainActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -18,29 +18,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var click = findViewById<Button>(R.id.click)
+
+        var click = findViewById<Button>(R.id.save)
         click.setOnClickListener(View.OnClickListener{
             callSecondActivity()
         })
+
+        responseFromSecondActiviy()
     }
     private fun callSecondActivity(){
-        var num1 = findViewById<EditText>(R.id.number_1)
-        var num2 = findViewById<EditText>(R.id.number_2)
-        var number1 = num1.text.toString().toInt()
-        var number2 = num2.text.toString().toInt()
-        var selected_operation=""
+        var name = findViewById<EditText>(R.id.name)
+        var email = findViewById<EditText>(R.id.email)
+        var department = findViewById<EditText>(R.id.department)
 
-        radioGroup = findViewById(R.id.radio_group)
-        var id: Int = radioGroup.checkedRadioButtonId
-        if (id != -1){
-            var radio: RadioButton = findViewById(id)
-            selected_operation = radio.text.toString()
-        }
+        var name_text =  name.text.toString()
+        var email_text = email.text.toString()
+        var department_text = department.text.toString()
+
+
         val second_activity=  Intent(this@MainActivity, SecondActivity::class.java).also {
-            it.putExtra("Number_1", number1)
-            it.putExtra("Number_2", number2)
-            it.putExtra("Operation",selected_operation)
+            it.putExtra("Name", name_text)
+            it.putExtra("Email", email_text)
+            it.putExtra("Department",department_text)
             startActivity(it)
+        }
+    }
+
+    private fun responseFromSecondActiviy(){
+        var name = intent.getStringExtra("Name")
+        var email = intent.getStringExtra("Email")
+        var department = intent.getStringExtra("Department")
+
+        var name_txt = findViewById<TextView>(R.id.name).apply {
+            text = name
+        }
+        var num2 = findViewById<TextView>(R.id.email).apply {
+            text = email
+        }
+        var operation_desc = findViewById<TextView>(R.id.department).apply {
+            text = department
         }
     }
 }
--- app/src/main/java/com/example/kotlin_app/SecondActivity.kt
+++ app/src/main/java/com/example/kotlin_app/SecondActivity.kt
@@ -3,6 +3,8 @@ package com.example.kotlin_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
@@ -10,37 +12,38 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        var number1 = intent.getIntExtra("Number_1",hashCode())
-        var number2 = intent.getIntExtra("Number_2",hashCode())
-        var operation = intent.getStringExtra("Operation")
+        var name = intent.getStringExtra("Name")
+        var email = intent.getStringExtra("Email")
+        var department = intent.getStringExtra("Department")
 
-        var num1 = findViewById<TextView>(R.id.number1).apply {
-            text = "Number:1 = "+number1
+        var name_txt = findViewById<TextView>(R.id.name).apply {
+            text = name
         }
-        var num2 = findViewById<TextView>(R.id.number2).apply {
-            text = "Number:2 = "+number2
+        var num2 = findViewById<TextView>(R.id.email).apply {
+            text = email
         }
-        var operation_desc = findViewById<TextView>(R.id.operation).apply {
-            text = "Selected operation is: "+ operation
+        var operation_desc = findViewById<TextView>(R.id.department).apply {
+            text = department
         }
-        number1 = number1.toInt()
-        number2 = number2.toInt()
-        var answer = 0
 
-        if(operation=="+"){
-            answer= number1+number2
-        }
-        if(operation=="-"){
-            answer= number1-number2
-        }
-        if(operation=="x"){
-            answer= number1*number2
-        }
-        if(operation=="/"){
-            answer= number1/number2
-        }
-        var result_desc = findViewById<TextView>(R.id.result).apply {
-            text ="RESULT = "+ answer
+        var save_button = findViewById<Button>(R.id.save)
+        save_button.setOnClickListener(View.OnClickListener {
+            callFirstAcivity()
+        })
+    }
+
+    private fun callFirstAcivity(){
+        val first_activity =  Intent(this@SecondActivity, MainActivity::class.java).also {
+            val name_txt=  findViewById<TextView>(R.id.name)
+            val name = name_txt.text.toString()
+            val email_txt=  findViewById<TextView>(R.id.email)
+            val email = email_txt.text.toString()
+            val department_txt=  findViewById<TextView>(R.id.department)
+            val department = department_txt.text.toString()
+            it.putExtra("Name", name)
+            it.putExtra("Email", email)
+            it.putExtra("Department",department)
+            startActivity(it)
         }
     }
 }
