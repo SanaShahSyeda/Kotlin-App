774c6fb5803a95d910dc79499386e5bbddd2a182
Syeda Sana <sanashaaah4@gmail.com>
2023-09-23 00:15:32 -0700

    image viewer app with explicit intent

--- app/src/main/java/com/example/kotlin_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_app/MainActivity.kt
@@ -3,10 +3,8 @@ package com.example.kotlin_app
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import android.widget.ImageView
 import android.widget.RadioGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -18,45 +16,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+          var cat = findViewById<ImageView>(R.id.cat)
+          var dog = findViewById<ImageView>(R.id.dog)
+          var coffee = findViewById<ImageView>(R.id.coffee)
+          var parrot = findViewById<ImageView>(R.id.parrot)
+          var android = findViewById<ImageView>(R.id.android)
+          var scenery = findViewById<ImageView>(R.id.scenery)
 
-        var click = findViewById<Button>(R.id.save)
-        click.setOnClickListener(View.OnClickListener{
-            callSecondActivity()
+        cat.setOnClickListener(View.OnClickListener {
+            var second_activity = Intent(this@MainActivity, SecondActivity::class.java).also {
+                it.putExtra("id",R.drawable.cat)
+                startActivity(it)
+            }
         })
-
-        responseFromSecondActiviy()
-    }
-    private fun callSecondActivity(){
-        var name = findViewById<EditText>(R.id.name)
-        var email = findViewById<EditText>(R.id.email)
-        var department = findViewById<EditText>(R.id.department)
-
-        var name_text =  name.text.toString()
-        var email_text = email.text.toString()
-        var department_text = department.text.toString()
-
-
-        val second_activity=  Intent(this@MainActivity, SecondActivity::class.java).also {
-            it.putExtra("Name", name_text)
-            it.putExtra("Email", email_text)
-            it.putExtra("Department",department_text)
-            startActivity(it)
+        dog.setOnClickListener(View.OnClickListener {
+            var second_activity = Intent(this@MainActivity, SecondActivity::class.java).also {
+                it.putExtra("id",R.drawable.dog)
+                startActivity(it)
+            }
+        })
+        coffee.setOnClickListener(View.OnClickListener {
+            var second_activity = Intent(this@MainActivity, SecondActivity::class.java).also {
+                it.putExtra("id",R.drawable.coffee)
+                startActivity(it)
+            }
+        })
+        parrot.setOnClickListener(View.OnClickListener {
+            var second_activity = Intent(this@MainActivity, SecondActivity::class.java).also {
+                it.putExtra("id",R.drawable.parrot)
+                startActivity(it)
+            }
+        })
+        android.setOnClickListener(View.OnClickListener {
+            var second_activity = Intent(this@MainActivity, SecondActivity::class.java).also {
+                it.putExtra("id",R.drawable.android)
+                startActivity(it)
+            }
+        })
+        scenery.setOnClickListener(View.OnClickListener {
+            var second_activity = Intent(this@MainActivity, SecondActivity::class.java).also {
+                it.putExtra("id",R.drawable.scenery)
+                startActivity(it)
+            }
+        })
         }
-    }
-
-    private fun responseFromSecondActiviy(){
-        var name = intent.getStringExtra("Name")
-        var email = intent.getStringExtra("Email")
-        var department = intent.getStringExtra("Department")
-
-        var name_txt = findViewById<TextView>(R.id.name).apply {
-            text = name
-        }
-        var num2 = findViewById<TextView>(R.id.email).apply {
-            text = email
-        }
-        var operation_desc = findViewById<TextView>(R.id.department).apply {
-            text = department
-        }
-    }
 }
--- app/src/main/java/com/example/kotlin_app/SecondActivity.kt
+++ app/src/main/java/com/example/kotlin_app/SecondActivity.kt
@@ -1,10 +1,8 @@
 package com.example.kotlin_app
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
@@ -12,39 +10,9 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        var name = intent.getStringExtra("Name")
-        var email = intent.getStringExtra("Email")
-        var department = intent.getStringExtra("Department")
-
-        var name_txt = findViewById<TextView>(R.id.name).apply {
-            text = name
-        }
-        var num2 = findViewById<TextView>(R.id.email).apply {
-            text = email
-        }
-        var operation_desc = findViewById<TextView>(R.id.department).apply {
-            text = department
-        }
-
-        var save_button = findViewById<Button>(R.id.save)
-        save_button.setOnClickListener(View.OnClickListener {
-            callFirstAcivity()
-        })
-    }
-
-    private fun callFirstAcivity(){
-        val first_activity =  Intent(this@SecondActivity, MainActivity::class.java).also {
-            val name_txt=  findViewById<TextView>(R.id.name)
-            val name = name_txt.text.toString()
-            val email_txt=  findViewById<TextView>(R.id.email)
-            val email = email_txt.text.toString()
-            val department_txt=  findViewById<TextView>(R.id.department)
-            val department = department_txt.text.toString()
-            it.putExtra("Name", name)
-            it.putExtra("Email", email)
-            it.putExtra("Department",department)
-            startActivity(it)
-        }
+        var img_result = findViewById<ImageView>(R.id.resulting_image)
+        var resourceID = intent.getIntExtra("id",0)
+        img_result.setImageResource(resourceID)
     }
 }
 
