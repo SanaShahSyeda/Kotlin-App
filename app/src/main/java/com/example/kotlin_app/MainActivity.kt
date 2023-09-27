5cf311cff9b9e62a10aba0d9db84104ce7331fdd
Syeda Sana <sanashaaah4@gmail.com>
2023-09-27 03:01:46 -0700

    a context menu applied to an image

--- app/src/main/java/com/example/kotlin_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_app/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.kotlin_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
@@ -21,37 +23,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var toolbar: Toolbar = findViewById(R.id.tool_bar)
+        var img : ImageView =  findViewById(R.id.hot_chocolate)
+        registerForContextMenu(img)
     }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean{
-        menuInflater.inflate(R.menu.options_menu,menu)
-        return true
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean{
-        when(item.itemId) {
-            R.id.main_courses -> {
-                    // Toast.makeText(this,"Main Courses item is selected", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this@MainActivity, Main_Courses::class.java).also{
-                        startActivity(it)
-                    }
-                    return true
-            }
-            R.id.appetizers-> {
-                // Toast.makeText(this,"Appetizers item is selected", Toast.LENGTH_SHORT).show()
-                val intent= Intent(this@MainActivity, Appetizers::class.java).also {
-                    startActivity(it)
-                }
-                return true
-            }
-            R.id.desserts -> {
-                // Toast.makeText(this,"Desserts item is selected", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this@MainActivity, Desserts::class.java).also {
-                    startActivity(it)
-                }
-                return true
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?) {
+         super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context_menu,menu)
     }
 }
 
