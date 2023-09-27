f4cf0aa88b2dcc956f33299be607459759c617f2
Syeda Sana <sanashaaah4@gmail.com>
2023-09-27 06:23:10 -0700

    a toolbar with icons using options_menu

--- app/src/main/java/com/example/kotlin_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_app/MainActivity.kt
@@ -1,37 +1,21 @@
 package com.example.kotlin_app
 
-import android.content.Intent
+
 import android.os.Bundle
-import android.view.ContextMenu
 import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.TextView
-import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-
-
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var img : ImageView =  findViewById(R.id.hot_chocolate)
-        registerForContextMenu(img)
+        var toolbar: Toolbar = findViewById(R.id.tool_bar)
     }
-    override fun onCreateContextMenu(
-        menu: ContextMenu?,
-        v: View?,
-        menuInfo: ContextMenu.ContextMenuInfo?) {
-         super.onCreateContextMenu(menu, v, menuInfo)
-        menuInflater.inflate(R.menu.context_menu,menu)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu,menu)
+        return true
     }
 }
 
